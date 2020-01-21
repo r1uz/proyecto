1711dcf1e5869b6d91436bb4b810e13564ef07e8
Mauricio Castillo <mcastillo.borquez@gmail.com>
2020-01-21 00:38:48 -0300

    creacion de ADO de producto

---
+++ Testeo/ADO/ProductoADO.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Testeo.ADO
+{
+    public class ProductoADO
+    {
+        private ProyectoEntities contexto = new ProyectoEntities();
+
+        public ProductoADO()
+        {
+
+        }
+        public int agregarProducto(Producto p)
+        {
+            contexto.Producto.Add(p);
+            return contexto.SaveChanges();
+        }
+
+        public Producto buscarProducto(int codigo)
+        {
+            return contexto.Producto.Find(codigo);
+        }
+
+        public int eliminarProducto(int codigo)
+        {
+            Producto p = contexto.Producto.Find(codigo);
+            contexto.Producto.Remove(p);
+            return contexto.SaveChanges();
+        }
+
+        public int actualizar(Producto nuevo)
+        {
+            Producto p = contexto.Producto.Find(nuevo.id_categoriap);
+            p = nuevo;
+            return contexto.SaveChanges();
+        }
+
+        public List<Producto> getProductos()
+        {
+            return contexto.Producto.ToList();
+        }
+    }
+}
